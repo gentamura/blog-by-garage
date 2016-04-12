26686394c36d726c76a2de95bf99c4a32c84a95c
Gen Tamura <gen.tamura84@gmail.com>
2016-04-12 18:26:14 +0900

    First init until test settings

---
+++ config/initializers/garage.rb
@@ -0,0 +1,20 @@
+Garage.configure {}
+
+Garage::TokenScope.configure do
+  register :public, desc: 'acessing publicly available data' do
+    access :read, User
+    access :write, User
+  end
+end
+
+Doorkeeper.configure do
+  orm :active_record
+  default_scopes :public
+  optional_scopes(*Garage::TokenScope.optional_scopes)
+
+  resource_owner_from_credentials do |routes|
+    User.find_by(email: params[:username])
+  end
+
+  grant_flows %w(password)
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :user do
+    sequence(:name) {|n| "user#{n}"}
+    email { "#{name}@example.com" }
+  end
+end
